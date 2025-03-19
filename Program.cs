dbb443d268cdc54d55ad47aecd3801177bc7a0ef
EudesNilton <eudes258@gmail.com>
2025-03-19 12:56:08 -0300

    Estudo váriaveis

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Dotnet;
+
+public class Program 
+{
+    public static void Main()
+    {
+        //Variaveis são locais para guardar informações
+
+        string name = "Eudes";
+        Console.WriteLine(name);
+
+        int age = 27;
+        Console.WriteLine(age);
+
+        double value = 300.0;
+        Console.WriteLine(value);
+
+        char character = 'a';
+        Console.WriteLine(character);
+        
+        bool boolean = true;
+        Console.WriteLine(boolean);
+    
+    }
+}
+
+
